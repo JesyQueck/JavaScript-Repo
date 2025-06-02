6037b3d1028bab6f00fe6affe900a18980c03fa9
JesyQueck <oyedokunolumuyiwa07@gmail.com>
2025-06-02 20:04:26 +0100

    commit5- 1

--- EventPlannerApp.js
+++ EventPlannerApp.js
@@ -1,19 +1,24 @@
 let eventPlanner = {
-     events:  [], 
-     addEvent (name, date, capacity, ){
-          const event = {
-               name: name,
-               date: date,
-               capacity: capacity,
-               attendees:[],
-          }
-     addAttendee(attendeeName){
-          if(attendeeName < this.capacity){
-               this.attendees.push(attendeeName);
-               return true;
-          }else{
-               console.log(`Sorry! This`)
-          }
-     }
-     }
-}
+    events: [],
+
+    addEvent(name, date, capacity) {
+        const event = {
+            name: name,
+            date: date,
+            capacity: capacity,
+            attendees: [],
+
+            addAttendee(attendeeName) {
+                if (this.attendees.length < this.capacity) {
+                    this.attendees.push(attendeeName);
+                    return true;
+                } else {
+                    console.log(`Sorry! The event "${this.name}" is full.`);
+                    return false;
+                }
+            }
+        };
+
+        this.events.push(event);
+    }
+};
